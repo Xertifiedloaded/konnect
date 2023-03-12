92982ba674ad56a8a9047729c454d7781638769d
xertifiedloaded <certifiedloaded@yahoo.com>
2023-03-12 09:38:12 +0100

    my changes

--- src/components/ui/header/header.js
+++ src/components/ui/header/header.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './header.module.css'
 import Logo from '../../../assets/images/Header-logo.png'
 import { Link } from 'react-router-dom'
+import Close from '../../../assets/images/icon-close-menu.svg'
+import Open from '../../../assets/images/icon-menu.svg'
 const Header = () => {
+    const [isMobile, setIsmobile] = useState(false)
+    const toggleMenu = () => {
+        setIsmobile(!isMobile)
+    }
     return (
         <>
             <header>
@@ -11,13 +17,17 @@ const Header = () => {
                         <img src={Logo} alt="logo" />
                         <p>wallet connect</p>
                     </div>
-                    <nav>
+                    <div onClick={toggleMenu} className={classes.mobileMenu}>
+                        {isMobile ? <img src={Close} alt="" /> : <img src={Open} alt="" />}
+                    </div>
+                    <nav className={`${isMobile ? classes.open : classes.close} && ${classes.nav}`}>
                         <Link to="/">
                             <button>
                                 Connect
                             </button>
                         </Link>
                     </nav>
+
                 </div>
             </header>
         </>
